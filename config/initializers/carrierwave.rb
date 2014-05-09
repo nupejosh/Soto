b5ab3ac6c07701376d35492bd8586d62a8b5b954
Joshua Smith <Josh@context.io>
2014-05-09 00:25:36 -0500

    took away ENW variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['AKIAIGJCR25W4LCRDMQQ'],       # required
-    :aws_secret_access_key  => ENV['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'],       # required
+    :aws_access_key_id      => 'AKIAIGJCR25W4LCRDMQQ',       # required
+    :aws_secret_access_key  => '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',       # required
     #:region                 => 'US Standard'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'Soto' # required
