f29f076d2d4949a8facb86a9cf3e3799f6333248
Joshua Smith <Josh@context.io>
2014-05-08 22:54:44 -0500

    Added a few gems and still trying to push to heroku

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+config.serve_static_assets = true
 
 module Soto
   class Application < Rails::Application
